4bc6ee94327baf47901db1649579fd836046a3a4
Jerome Trezeguet <jerome.trezeguet@lapiscine.pro>
2024-08-09 14:35:28 +0200

    Home_MessageYouAreLogged

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,13 +13,14 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
 
-
        if( $this->isGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('admin_articles_list_db');
-       }
+       } // si tu as le role ADMIN  alors redirection vers admin_articles_list_db
+        //si tu as le role USER alors redirection vers
 
        if ($this->isGranted('ROLE_USER')) {
            return $this->redirectToRoute('home');
+           //si tu as le role USER alors redirection vers home (chemin)
        }
 
         // get the login error if there is one
