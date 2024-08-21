110ed0431de0c9e47c772d24a09e6952e2d832d4
Jerome Trezeguet <jerome.trezeguet@lapiscine.pro>
2024-08-21 11:13:37 +0200

    merge branch

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -7,9 +7,13 @@ use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 =======
 >>>>>>> 8588bb4 (user_insert_form)
+=======
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+>>>>>>> f1812c5 (uploadfiles_comments)
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -24,12 +28,18 @@ class ArticleType extends AbstractType
             ->add('description')
             ->add('image')
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> f1812c5 (uploadfiles_comments)
             ->add('image', FileType::class, [
                 // demande à symfony de ne pas gérer automatiquement le champs image, je prends la main
                 'mapped' => false,
             ])
+<<<<<<< HEAD
 =======
 >>>>>>> 8588bb4 (user_insert_form)
+=======
+>>>>>>> f1812c5 (uploadfiles_comments)
             ->add('isPublished')
             ->add('category', EntityType::class, [
                 'class' => Category::class,
